fd3f4d05432446f9d6e97656f6e829d80b76a2fc
Vasilis Vamvakas <billvamva@hotmail.com>
2025-07-29 22:42:53 +0300

    updating with memory profiling

--- arena.c
+++ arena.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-arena new_arena(ptrdiff_t cap)
+arena new_arena(ptrdiff_t cap, mem_profile_t* profile)
 {
     arena a = { 0 };
     a.start = malloc(cap);
@@ -13,6 +13,7 @@ arena new_arena(ptrdiff_t cap)
     }
     a.beg = a.start;
     a.end = a.beg ? a.beg + cap : 0;
+    a.profile = profile;
     return a;
 }
 
@@ -22,6 +23,9 @@ void free_arena(arena* a)
         free(a->start);
         a->beg = 0;
         a->end = 0;
+    }
+    if (a->profile) {
+        free(a->profile);
     }
 }
 
@@ -34,5 +38,32 @@ void* alloc(arena* a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count)
     }
     void* p = a->beg + padding;
     a->beg += padding + count * size;
+
+    if (a->profile) {
+        // here we need to record stuff
+    }
+
     return memset(p, 0, count * size);
+}
+
+static uint8_t record_call_stack(uint64_t* dst, uint64_t cap)
+{
+    uintptr_t* rbp = __builtin_frame_address(0);
+
+    uint64_t len = 0;
+
+    while (rbp != 0 && ((uint64_t)rbp & 7) == 0 && *rbp != 0) {
+        const uintptr_t rip = *(rbp + 1);
+        rbp = (uintptr_t*)*rbp;
+
+        // `rip` points to the return instruction in the caller, once this call is
+        // done. But: We want the location of the call i.e. the `call xxx`
+        // instruction, so we subtract one byte to point inside it, which is not
+        // quite 'at' it, but good enough.
+        dst[len++] = rip - 1;
+
+        if (len >= cap)
+            return len;
+    }
+    return len;
 }
--- arena.h
+++ arena.h
@@ -2,16 +2,34 @@
 #define ARENA_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+typedef struct mem_profile_t mem_profile_t;
 
 typedef struct arena {
     char* start;
     char* beg;
     char* end;
+    mem_profile_t* profile;
 } arena;
+
+typedef struct mem_record_t {
+    uint64_t in_use_space, in_use_objects, alloc_space, alloc_objects;
+    uint64_t* call_stack;
+    uint64_t call_stack_len;
+} mem_record_t;
+
+struct mem_profile_t {
+    mem_record_t* records;
+    uint64_t records_len;
+    uint64_t records_cap;
+    uint64_t in_use_space, in_use_objects, alloc_space, alloc_objects;
+    arena* arena;
+};
 
 #define new(a, t, n) (t*)alloc(a, sizeof(t), _Alignof(t), n)
 
-arena new_arena(ptrdiff_t cap);
+arena new_arena(ptrdiff_t cap, mem_profile_t* profile);
 void free_arena(arena* a);
 void* alloc(arena* a, ptrdiff_t size, ptrdiff_t alignment, ptrdiff_t count);
 
